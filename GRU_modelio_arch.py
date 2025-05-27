ae57626c166da08d6497ff4fe157c7f2894ca4a3
Šarunas Kontautas <kontautas.s@gmail.com>
2025-05-27 14:02:13 +0300

    Add files via upload

--- GRU_modelio_arch.py
+++ GRU_modelio_arch.py
@@ -11,7 +11,7 @@ from tqdm import tqdm
 import os
 from torch.optim.lr_scheduler import ReduceLROnPlateau
 
-#____________________________Modelis: GRU________________________________
+# GRU Modelis
 class GRUModel(nn.Module):
     def __init__(self, input_size, hidden_sizes=[256, 96, 64], output_size=1, dropout_rate=0.3):
         super(GRUModel, self).__init__()
@@ -45,7 +45,7 @@ class GRUModel(nn.Module):
 
         return output
 
-#________________________Modelio treniravimo funkcija________________________________
+# Modelio treniravimo_Funkcija
 def train_model(
     model,
     train_loader,
@@ -53,88 +53,97 @@ def train_model(
     optimizer,
     criterion,
     device,
-    epochs=6,
-    best_model_path="geriausias_GRU_modelis.pth",
-    best_val_loss=None,
-    patience=10
+    epochs = 6,                                                                                # Kiek epochu mokyti
+    best_model_path = "geriausias_GRU_modelis.pth",                                            # Issaugomo geriausio modelio pavadinimas 
+    best_val_loss = None,
+    patience = 10                                                                              # Kiek epchų laukti be pagerėjimo
 ):
     if best_val_loss is None:
-        best_val_loss = float('inf')
-
-    scheduler = ReduceLROnPlateau(optimizer, mode='min', factor=0.00001, patience=3)
+        best_val_loss = float('inf')                                                           # Nustatomas į begalybę, pirmo modelio issaugojimui
+   # Automatinis learning rate mazinimas, jei val_loss nustoja gerėti
+    scheduler = ReduceLROnPlateau(optimizer,
+                                mode='min',
+                                factor=0.00001,                                               # Nustatymas - kiek sumazinti learning rate
+                                patience=3)                                                   # Nustatymas - kiek epochų laukti be pagerejimo
     train_losses = []
     val_losses = []
-    epochs_no_improve = 0
+    epochs_no_improve = 0                                                                     # Skaičiuojama kiek epochų be pagerėjimo
 
-    for epoch in range(epochs):
+    # Mokymo ciklas
+    for epoch in range(epochs):                                                               # Mokymo ciklas - eina per visas epochas
         model.train()
         train_loss = 0
-        for X_batch, y_batch in tqdm(train_loader, desc=f'Epoch {epoch+1}/{epochs}', leave=False):
+        for X_batch, y_batch in tqdm(train_loader,                                            # Paimame duomenu partija (Batch)
+                                      desc=f'Epoch {epoch+1}/{epochs}',
+                                      leave=False):
             X_batch, y_batch = X_batch.to(device), y_batch.to(device)
-            optimizer.zero_grad()
-            y_pred = model(X_batch)
-            loss = criterion(y_pred, y_batch)
-            loss.backward()
-            optimizer.step()
-            train_loss += loss.detach().item()
+            optimizer.zero_grad()                                                             # Isvalomi gradientai
+            y_pred = model(X_batch)                                                           # Atliekama prognoze
+            loss = criterion(y_pred, y_batch)                                                 # Apskaiciuojamas nuostolis
+            loss.backward()                                                                   # Atgalinis zingsnis
+            optimizer.step()                                                                  # Atnaujinami modelio svoriai
+            train_loss += loss.detach().item()                                                # Issaugomas nuostolis, vėliasniam epochos vidurkio apskaiciavimui
 
         train_loss /= len(train_loader)
         train_losses.append(train_loss)
 
+        #VALIDACIJA
         model.eval()
         val_loss = 0
-        with torch.no_grad():
+        with torch.no_grad():                                                               # Isjungiami gradientai - greitesne validacija
             for X_batch, y_batch in val_loader:
                 X_batch, y_batch = X_batch.to(device), y_batch.to(device)
                 y_pred = model(X_batch)
-                val_loss += criterion(y_pred, y_batch).item()
+                val_loss += criterion(y_pred, y_batch).item()                               # Skaiciuojamas validacijos loss.
 
-        scheduler.step(val_loss)
-        val_loss /= len(val_loader)
+        scheduler.step(val_loss)                                                            # Tikriname ar yra pagerejimas. Jei nera 3 epochas - lr mazinamas
+        val_loss /= len(val_loader)                                                         # Apskaiciuojamas vidutinis nuostolis per epocha
         val_losses.append(val_loss)
 
         print(f"Epoch {epoch+1}/{epochs}, Train Loss: {train_loss:.4f}, Val Loss: {val_loss:.4f}")
 
-        if val_loss < best_val_loss:
+        # Pasitikrinam ar pagerejo validacijos losas
+        if val_loss < best_val_loss:                                                        # Jei esamas val_loss geresnis uz buvusi - issaugome
             best_val_loss = val_loss
             epochs_no_improve = 0
             torch.save(model.state_dict(), best_model_path)
             print(f" Geriausias modelis išsaugotas su nuostoliu: {best_val_loss:.4f}")
         else:
             epochs_no_improve += 1
-            print(f"Jokio pagerėjimo {epochs_no_improve}/{patience} epochas")
+            print(f"Jokio pagerėjimo {epochs_no_improve}/{patience} epochas")               # Jei nera pagerejimo, skaiciuojame kiek epochu negereja
 
-        if epochs_no_improve >= patience:
+        # prieslaikinis stabdymas
+        if epochs_no_improve >= patience:                                                   # Jei negerėja ilgiau nei nustatyta - ankstyvas stabdymas
             print(f"Ankstyvas stabdymas po {epoch+1} epokų validacijos nuostolis nepagerėjo {patience} epokas.")
             break
 
-    return train_losses, val_losses
+    return train_losses, val_losses                                                         # Grazinami sarasai tolimesnei analizei.
 
-#______________________________Testavimo funkcija_______________________
+# Testavimo funkcija
 def test_model(model_class, model_path, test_loader, device, criterion,
                original_df_column=None, save_csv_path=None):
 
-    example_batch = next(iter(test_loader))[0]
+    example_batch = next(iter(test_loader))[0]                                              # pasiimam pirma batcha iejimo duomenu dydziui
     input_size = example_batch.shape[2]
     model = model_class(input_size=input_size).to(device)
-    model.load_state_dict(torch.load(model_path))
+    model.load_state_dict(torch.load(model_path))                                           # susikeliam apmokyto modelio svorius
     model.eval()
 
-    all_preds = []
-    all_targets = []
+    all_preds = []                                                                          # Masyvas prognozuotoms reiksmems
+    all_targets = []                                                                        # Masyvas tikroms reiksmems
 
-    with torch.no_grad():
+    with torch.no_grad():                                                                   # Neskaiciuojame gradientu - mazesnis resursu poreikis
         for X_batch, y_batch in test_loader:
             X_batch, y_batch = X_batch.to(device), y_batch.to(device)
             y_pred = model(X_batch)
-            all_preds.append(y_pred.cpu())
+            all_preds.append(y_pred.cpu())                                                  # Reikšmės perkeliamos i CPU pries konvertuojant i numpy
             all_targets.append(y_batch.cpu())
 
-    preds = torch.cat(all_preds, dim=0).numpy()
-    targets = torch.cat(all_targets, dim=0).numpy()
+    preds = torch.cat(all_preds, dim=0).numpy()                                             # Sujungiamos visos prognozes i numpy masyva
+    targets = torch.cat(all_targets, dim=0).numpy()                                         # Sujungiamos visos tikros reiksmes i numpy masyva
 
-    mse = mean_squared_error(targets, preds)
-    mae = mean_absolute_error(targets, preds)
+    mse = mean_squared_error(targets, preds)                                                # Skaiciuojamas kvadratinis nuostolis,
+    mae = mean_absolute_error(targets, preds)                                               # Skaiciuojamas vidutinis absoliutus skirtumas
     print(f"Test MSE: {mse:.6f}")
     print(f"Test MAE: {mae:.6f}")
 
@@ -162,7 +171,7 @@ def test_model(model_class, model_path, test_loader, device, criterion,
 
     return preds_unscaled, targets_unscaled
 
-#_______________Predikcija vienai sekai________________________________
+# LV_OUT_prognozes funkcija
 def predict_is_duom_eilutes(x, model_path, device="cpu"):
     input_size = x.shape[1]
     hidden_sizes = [256, 96, 64]
